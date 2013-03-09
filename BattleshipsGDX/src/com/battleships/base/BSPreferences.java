eeb832a7e1edacdc4669c1772319975c50578fb1
kacperpl1 <kacperpl1@gmail.com>
2013-03-09 09:32:55 +0100

    Fixed android preferences

--- BattleshipsGDX/src/com/battleships/base/BSPreferences.java
+++ BattleshipsGDX/src/com/battleships/base/BSPreferences.java
@@ -7,27 +7,24 @@ public class BSPreferences {
     private static final String PREFS_NAME = "config";
     private static final String PREFS_SERVER_IP = "server_ip";
     private static final String PREFS_USER_NAME = "user_name";
-    
-    protected Preferences getPrefs() {
-    	return Gdx.app.getPreferences( PREFS_NAME );
-    }
+    private static final Preferences prefs = Gdx.app.getPreferences( PREFS_NAME );
     
     public void setUserName(String uName) {
-    	getPrefs().putString(PREFS_USER_NAME, uName);
-    	getPrefs().flush();
+    	prefs.putString(PREFS_USER_NAME, uName);
+    	prefs.flush();
     }
     
     public String getUserName() {
-    	return getPrefs().getString(PREFS_USER_NAME, "Battleship");
+    	return prefs.getString(PREFS_USER_NAME, "Battleship");
     }
     
     public void setServerIp(String sIP) {
-    	getPrefs().putString(PREFS_SERVER_IP, sIP);
-    	getPrefs().flush();
+    	prefs.putString(PREFS_SERVER_IP, sIP);
+    	prefs.flush();
     }
     
     public String getServerIp() {
-    	return getPrefs().getString(PREFS_SERVER_IP, "192.168.198.185");
+    	return prefs.getString(PREFS_SERVER_IP, "192.168.198.185");
     }
     
 }
