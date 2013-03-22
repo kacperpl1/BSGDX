0a1313417f0bf951d11957778457ca1f3a71faa0
kacperpl1 <kacperpl1@gmail.com>
2013-03-22 09:35:17 +0100

    removed test code

--- BattleshipsGDX/src/com/battleships/base/NetworkGameScreen.java
+++ BattleshipsGDX/src/com/battleships/base/NetworkGameScreen.java
@@ -55,8 +55,8 @@ public class NetworkGameScreen extends GameScreen{
 					shipMap.put((short)player.getSlotNumber(), new PlayerShip("blue", 0, -768, player.getSlotNumber()));
 				}
 			}
-			//localPlayerShip = shipMap.get((short)lobbyClient.getPlayer().getSlotNumber());
-			localPlayerShip = shipMap.get((short)3);
+			localPlayerShip = shipMap.get((short)lobbyClient.getPlayer().getSlotNumber());
+			//localPlayerShip = shipMap.get((short)3);
 
 			//send initial direction packet
 			lobbyClient.sendDirection(playerData);
